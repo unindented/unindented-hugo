088c0366ef9f847db79512e46070c8143684c0cd
Daniel Perez Alvarez <daniel@unindented.org>
2020-11-18 15:25:51 -0800

    Build for the right environment in workflows

--- .github/actions/rsync/entrypoint.sh
+++ .github/actions/rsync/entrypoint.sh
@@ -10,13 +10,23 @@ mkdir -p "${SSH_PATH}"
 
 if [ -z "${SSH_PRIVATE_KEY}" ];
 then
-  echo ::set-output name=status::'Action did not find the SSH_PRIVATE_KEY secret.'
+  echo ::set-output name=status::'Could not find secret with SSH key.'
   exit 1
-else
-  printf '%b\n' "${SSH_PRIVATE_KEY}" > "${SSH_PATH}/key"
 fi
+
+printf '%b\n' "${SSH_PRIVATE_KEY}" > "${SSH_PATH}/key"
 
 chmod 700 "${SSH_PATH}"
 chmod 600 "${SSH_PATH}/key"
 
-sh -c "rsync ${INPUT_OPTIONS} -e 'ssh -i ${SSH_PATH}/key' '${GITHUB_WORKSPACE}/${INPUT_SOURCE}' '${SSH_USERNAME}@${SSH_HOSTNAME}:${INPUT_TARGET}'"
+sh -c "rsync ${INPUT_OPTIONS} -e 'ssh -i ${SSH_PATH}/key' '${GITHUB_WORKSPACE}/${INPUT_SOURCE}' '${INPUT_TARGET}'"
+
+status=$?
+
+if [ ${status} -ne 0 ];
+then
+  echo "::set-output name=status::'There was an issue syncing the content.'"
+  exit 1
+else
+  echo "::set-output name=status::'Content synced successfully.'"
+fi
