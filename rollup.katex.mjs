e1ee53fec1d4007e1b43532616ab77553366c8b9
Daniel Perez Alvarez <danielalvarez@twitter.com>
2022-03-13 21:53:47 -0700

    Bump KaTeX to 0.15.2

---
+++ rollup.katex.mjs
@@ -0,0 +1,13 @@
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
+
+export default [
+  {
+    input: "contrib/auto-render.mjs",
+    output: {
+      file: "katex.min.mjs",
+      format: "esm",
+    },
+    plugins: [nodeResolve(), terser()],
+  },
+];
