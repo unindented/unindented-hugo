357a38365c57e9614e1950551fb7d9f00b3778d0
Daniel Perez Alvarez <daniel@unindented.org>
2023-01-06 20:49:11 -0800

    Tweak templates again

--- tailwind.typography.js
+++ tailwind.typography.js
@@ -36,6 +36,14 @@ const typographyModifiers = {
           marginBottom: em(8, 14),
           lineHeight: round(20 / 14),
         },
+        details: {
+          marginTop: em(20, 12),
+          marginBottom: em(20, 12),
+          paddingTop: em(8, 12),
+          paddingRight: em(12, 12),
+          paddingBottom: em(8, 12),
+          paddingLeft: em(12, 12),
+        },
         kbd: {
           borderRadius: rem(4),
           fontSize: em(12, 14),
@@ -89,6 +97,14 @@ const typographyModifiers = {
           marginTop: em(24, 16),
           marginBottom: em(8, 16),
           lineHeight: round(24 / 16),
+        },
+        details: {
+          marginTop: em(24, 14),
+          marginBottom: em(24, 14),
+          paddingTop: em(12, 14),
+          paddingRight: em(16, 14),
+          paddingBottom: em(12, 14),
+          paddingLeft: em(16, 14),
         },
         kbd: {
           borderRadius: rem(6),
@@ -144,6 +160,14 @@ const typographyModifiers = {
           marginBottom: em(8, 18),
           lineHeight: round(28 / 18),
         },
+        details: {
+          marginTop: em(32, 16),
+          marginBottom: em(32, 16),
+          paddingTop: em(16, 16),
+          paddingRight: em(24, 16),
+          paddingBottom: em(16, 16),
+          paddingLeft: em(24, 16),
+        },
         kbd: {
           borderRadius: rem(6),
           fontSize: em(16, 18),
@@ -198,6 +222,14 @@ const typographyModifiers = {
           marginBottom: em(12, 20),
           lineHeight: round(32 / 20),
         },
+        details: {
+          marginTop: em(36, 18),
+          marginBottom: em(36, 18),
+          paddingTop: em(20, 18),
+          paddingRight: em(24, 18),
+          paddingBottom: em(20, 18),
+          paddingLeft: em(24, 18),
+        },
         kbd: {
           borderRadius: rem(8),
           fontSize: em(18, 20),
@@ -251,6 +283,14 @@ const typographyModifiers = {
           marginTop: em(40, 24),
           marginBottom: em(16, 24),
           lineHeight: round(36 / 24),
+        },
+        details: {
+          marginTop: em(40, 20),
+          marginBottom: em(40, 20),
+          paddingTop: em(24, 20),
+          paddingRight: em(32, 20),
+          paddingBottom: em(24, 20),
+          paddingLeft: em(32, 20),
         },
         kbd: {
           borderRadius: rem(8),
@@ -310,6 +350,13 @@ const typographyDefault = {
         borderStyle: "dotted",
         borderTopWidth: "0.25rem",
       },
+      details: {
+        borderColor: "var(--tw-prose-hr)",
+        borderWidth: "1px",
+      },
+      "details summary": {
+        cursor: "pointer",
+      },
       kbd: {
         borderColor: "var(--tw-prose-hr)",
         borderWidth: "1px",
@@ -328,6 +375,9 @@ const typographyDefault = {
       },
       "samp::after": {
         content: '"`"',
+      },
+      "tbody td": {
+        verticalAlign: "top",
       },
       ".admonition": {
         borderLeftWidth: "0.25rem",
