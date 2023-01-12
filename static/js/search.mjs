69c605e011a305855967670afe04d0c9ee5ca947
Daniel Perez Alvarez <daniel@unindented.org>
2023-01-12 00:11:47 -0800

    Highlight search results

--- static/js/search.mjs
+++ static/js/search.mjs
@@ -4,6 +4,7 @@ const fuseOptions = {
   includeMatches: true,
   includeScore: true,
   shouldSort: true,
+  useExtendedSearch: true,
   keys: [
     { name: "title", weight: 0.8 },
     { name: "content", weight: 0.7 },
@@ -12,29 +13,38 @@ const fuseOptions = {
   ],
 };
 
-const searchResult = ({ title, content, permalink }) => `
-<article class="border-b border-gray-200 py-2 last:border-b-0 dark:border-gray-700 sm:py-4">
-  <h3>
-    <a
-      href="${permalink}"
-      class="text-2xl font-bold text-gray-700 hover:text-blue-700 focus:text-blue-700 dark:text-gray-300 dark:hover:text-blue-300 dark:focus:text-blue-300"
-      >${title}</a
-    >
-  </h3>
-  <p class="mt-2 truncate text-base text-gray-600 dark:text-gray-400">${content}</p>
-</article>
-`;
+const highlightText = (str, ranges) => {
+  const sortedRanges = [...ranges].sort(([aStart], [bStart]) => aStart - bStart);
 
-const searchResults = (results) => `
-<h2 class="sr-only">Search results</h2>
-${results.map(({ item }) => searchResult(item)).join('\n')}
-`;
+  const combinedRanges = sortedRanges.reduce((acc, next) => {
+    if (acc.length === 0 || acc[acc.length - 1][1] < next[0]) {
+      acc.push(next);
+    } else {
+      const prev = acc.pop();
+      acc.push([prev[0], Math.max(prev[1], next[1])]);
+    }
+    return acc;
+  }, []);
+
+  const { acc: highlightedRanges } = combinedRanges.reduce(
+    ({ acc, lastIndex }, [rangeStart, rangeEnd], rangeIndex) => {
+      const nextIndex = rangeEnd + 1;
+      acc.push(str.substring(lastIndex, rangeStart), "<u>", str.substring(rangeStart, nextIndex), "</u>");
+      if (rangeIndex === combinedRanges.length - 1) {
+        acc.push(str.substring(nextIndex));
+      }
+      return { acc, lastIndex: nextIndex };
+    },
+    { acc: [], lastIndex: 0 }
+  );
+
+  return highlightedRanges.join("");
+};
 
 export default class Search {
   constructor({ index, dialog, dialogInput, dialogConfirmButton, dialogResults }) {
     this.fuse = new Fuse(index, fuseOptions);
 
-    this.index = index;
     this.dialog = dialog;
     this.dialogInput = dialogInput;
     this.dialogConfirmButton = dialogConfirmButton;
@@ -66,7 +76,54 @@ export default class Search {
   async execute() {
     const { value } = this.dialogInput;
     const results = this.fuse.search(value);
-    const html = results.length > 0 ? searchResults(results) : '';
+    const html = results.length > 0 ? this.renderSearchResults(results) : "";
     this.dialogResults.innerHTML = html;
+  }
+
+  renderSearchResults(results) {
+    return `
+  <h2 class="sr-only">Search results</h2>
+  ${results.map((result) => this.renderSearchResult(result)).join("\n")}
+  `;
+  }
+
+  renderSearchResult(result) {
+    const highlightedResult = this.highlightSearchResult(result);
+    const { title, content, permalink } = highlightedResult;
+
+    return `
+<article class="border-b border-gray-200 py-2 last:border-b-0 dark:border-gray-700 sm:py-4">
+  <h3>
+    <a
+      href="${permalink}"
+      class="text-2xl font-bold text-gray-700 hover:text-blue-700 focus:text-blue-700 dark:text-gray-300 dark:hover:text-blue-300 dark:focus:text-blue-300"
+      >${title}</a
+    >
+  </h3>
+  <p class="mt-2 truncate text-base text-gray-600 dark:text-gray-400">${content}</p>
+</article>
+`;
+  }
+
+  highlightSearchResult({ item, matches }) {
+    return matches.reduce((acc, match) => this.updateResult(acc, match), structuredClone(item));
+  }
+
+  updateResult(result, { key, value, indices, refIndex }) {
+    const path = key.split(".");
+    const newValue = highlightText(value, indices);
+
+    for (let i = 0, l = path.length, obj = result; i < l; i++) {
+      const p = path[i];
+      if (i < l - 1) {
+        obj = obj[p];
+      } else if (refIndex == null) {
+        obj[p] = newValue;
+      } else {
+        obj[p][refIndex] = newValue;
+      }
+    }
+
+    return result;
   }
 }
