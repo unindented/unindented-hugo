e9f6301d930b10cc8c121f86467b7f4730b704f8
Daniel Perez Alvarez <daniel@unindented.org>
2023-07-07 16:06:26 -0700

    Add movies and music pages

--- tailwind.utils.js
+++ tailwind.utils.js
@@ -1,5 +1,33 @@
 const plugin = require("tailwindcss/plugin");
 
+const animationTimeline = plugin(({ matchUtilities }) => {
+  matchUtilities({
+    "animation-timeline": (value) => ({
+      "animation-timeline": `--${value}`,
+    }),
+  });
+});
+
+const boxReflect = plugin(({ matchUtilities, theme }) => {
+  matchUtilities(
+    {
+      "box-reflect": (value) => ({
+        "--tw-box-reflect-length": value,
+        "-webkit-box-reflect": "below var(--tw-box-reflect-length) linear-gradient(var(--tw-gradient-stops))",
+      }),
+    },
+    { values: theme("spacing") }
+  );
+});
+
+const perspective = plugin(({ matchUtilities }) => {
+  matchUtilities({
+    perspective: (value) => ({
+      perspective: value,
+    }),
+  });
+});
+
 const shapeRendering = plugin(({ addUtilities }) => {
   addUtilities({
     ".shape-auto": {
@@ -17,6 +45,26 @@ const shapeRendering = plugin(({ addUtilities }) => {
   });
 });
 
+const viewTimeline = plugin(({ addUtilities, matchUtilities }) => {
+  addUtilities({
+    ".view-timeline-block": {
+      "view-timeline-axis": "block",
+    },
+    ".view-timeline-inline": {
+      "view-timeline-axis": "inline",
+    },
+  });
+  matchUtilities({
+    "view-timeline-name": (value) => ({
+      "view-timeline-name": `--${value}`,
+    }),
+  });
+});
+
 module.exports = {
+  animationTimeline,
+  boxReflect,
+  perspective,
   shapeRendering,
+  viewTimeline,
 };
